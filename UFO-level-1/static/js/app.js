465ffbf1fbc71e71bd6f51b8b85b74ee1586e61d
unknown <jtslater2@gmail.com>
2021-02-20 20:29:18 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,15 +13,29 @@ class is form-control  id is datetime
 
 button id is filter-btn
 
+Object.entries(filt_md[0]).forEach(([key, value]) => {
+    metadata.append("p").text(key +" : "+value);
+    //metadata.append("p").text('${key}:${value}');
+
+
+
 
 */
 // Read json
 var tableData = data;
 var columns = ["datetime", "city", "state", "country", "shape", "comments"];
 console.log(columns);
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+
+tableData.forEach((view) => {
+    var addrow = tbody.append("tr")
+    //console.log(row)
+    
 
 
-tableData.forEach(())
-
+});
+//console.log(row)
 
 
