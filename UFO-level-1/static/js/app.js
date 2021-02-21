6923ef929e4e0a4dd3515794660e859f50b23d24
unknown <jtslater2@gmail.com>
2021-02-20 20:00:49 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
-// from data.js
-var tableData = data;
+//var tableData = data;
+/*
+YOUR CODE HERE!
+Notes:
 
-// YOUR CODE HERE!
+read json
+forEach command to append list 
+
+
+div class is form-group
+ul class is list-group
+class is form-control  id is datetime
+
+button id is filter-btn
+
+
+*/
+// Read json
+var tableData = data;
+var columns = ["datetime", "city", "state", "country", "shape", "comments"];
+console.log(columns);
+
+
+tableData.forEach(())
+
+
+
